5b5153c61308a71010ce53cb2539d336600eed3f
Luciano Otto Rodrigues <104655752+Lucianoottor@users.noreply.github.com>
2023-10-23 20:23:05 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 // Tema: Rede UP - Pedidos de Eletrônicos e Periféricos 
-// Structs - Clientes, Endereco, Produtos
+// Structs - Clientes, Enderecos, Produtos
 // Painéis - Cadastrar conta, Excluir/Desativar conta, Realizar pedido, Verificar conta, Acompanhar pedido, Verificar Estoque
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,49 +30,64 @@ struct clientes {
 };
 
 // Funções do programa
-void cadastrarConta(struct clientes *Clientes, int *numClientes) {
+void cadastrarConta(struct clientes *Clientes, int *numClientes, struct enderecos *Enderecos) {
     struct clientes *novo_cliente = (struct clientes*)realloc(Clientes, (*numClientes + 1) * sizeof(struct clientes));
 
     Clientes = novo_cliente;
     (*numClientes)++;
 
 
-printf("Digite o nome do cliente: ");
+    printf("Digite o nome do cliente: \n");
     scanf("%s", Clientes[*numClientes - 1].nome_cliente);
-    printf("Digite o e-mail do cliente: ");
+    printf("Digite o e-mail do cliente: \n");
     scanf("%s", Clientes[*numClientes - 1].email_cliente);
-    printf("Digite o CPF do cliente: ");
+    printf("Digite o CPF do cliente: \n");
     scanf("%s", Clientes[*numClientes - 1].cpf_cliente);
+    printf("Insira a UF (unidade federal): \n");
+    scanf("%s", Enderecos[*numClientes -1].strEstado);
+    printf("Insira a cidade: \n");
+    scanf("%s", Enderecos[*numClientes -1].strCidade);
+    printf("Insira o bairro: \n");
+    scanf("%s", Enderecos[*numClientes -1].strBairro);
+    printf("Insira o logradouro: \n");
+    scanf("%s", Enderecos[*numClientes -1].strRua);
+    printf("Insira o número da residência: \n");
+    scanf("%s", Enderecos[*numClientes -1].num_res);
 
     FILE *banco_de_dados = fopen("banco_de_dados.txt", "a+");
-    fprintf(banco_de_dados, "%s\n%s\n%s\n", Clientes[*numClientes - 1].nome_cliente, 
-                                          Clientes[*numClientes - 1].email_cliente, 
-                                          Clientes[*numClientes - 1].cpf_cliente);
+    fprintf(banco_de_dados, "%s\n%s\n%s\n", Clientes[*numClientes - 1].nome_cliente, Clientes[*numClientes - 1].email_cliente, Clientes[*numClientes - 1].cpf_cliente);
     fclose(banco_de_dados);
 
     printf("Cliente cadastrado com sucesso!\n");
-
 }
 
 
 void excluirConta(struct clientes Clientes[], int *numClientes) {
-    // Implemente a função de exclusão de conta aqui
+    char *consulta_cpf;
+    printf("Digite o CPF do cliente que será excluído: \n");
+    scanf("%s", consulta_cpf);
+    
+    FILE *banco_de_dados = fopen("banco_de_dados.txt", "a+");
+    if(fgets(consulta_cpf, sizeof(consulta_cpf), banco_de_dados));
 }
 
 void realizarPedido(struct clientes Clientes[], int numClientes, struct produtos Produtos[], int numProdutos) {
-    // Implemente a função de realização de pedido aqui
+
 }
 
 void verificarConta(struct clientes Clientes[], int numClientes) {
-    // Implemente a função de verificação de conta aqui
+  
 }
 
+
 void acompanharPedido(struct clientes Clientes[], int numClientes) {
-    // Implemente a função de acompanhamento de pedido aqui
+ 
+}
+
+void adicionarEstoque(struct produtos Produtos[], int numProdutos){
 }
 
 void verificarEstoque(struct produtos Produtos[], int numProdutos) {
-    // Implemente a função de verificação de estoque aqui
 }
 
 int main() {
@@ -84,9 +99,6 @@ int main() {
     struct clientes *Clientes = (struct clientes*)malloc(sizeof(struct clientes));
     int numClientes = 0;
     int numProdutos = 0;
-
-    // Manipular arquivo
-
     
     // Menu da da aplicação
     int input_usuario;
@@ -98,13 +110,14 @@ int main() {
         printf("4. Verificar Conta\n");
         printf("5. Acompanhar Pedido\n");
         printf("6. Verificar Estoque\n");
+        printf("7. Aumentar Estoque\n");
         printf("0. Sair\n");
         printf("Selecione uma opção: ");
         scanf("%d", &input_usuario);
         
         switch(input_usuario) {
             case 1:
-                cadastrarConta(Clientes, &numClientes);
+                cadastrarConta(Clientes, &numClientes, Enderecos);
                 break;
             case 2:
                 excluirConta(Clientes, &numClientes);
@@ -121,6 +134,8 @@ int main() {
             case 6:
                 verificarEstoque(Produtos, numProdutos);
                 break;
+            case 7:
+                adicionarEstoque(Produtos, numProdutos);
             case 0:
                 printf("Encerrando programa...\n");
                 break;
